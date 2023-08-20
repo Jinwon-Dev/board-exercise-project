9025f97200dc27afe0806cf4775f5b8d5e5d9bc5
Jinwon-Dev <yjw9424@naver.com>
2023-08-21 01:11:40 +0900

    feat: BoardType Description 추가

--- src/main/java/com/mailplug/homework/domain/board/BoardType.java
+++ src/main/java/com/mailplug/homework/domain/board/BoardType.java
@@ -7,6 +7,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.StringUtils;
 
+/**
+ * Enum으로 게시판을 정의한 이유
+ *
+ * : 확장이 불가능하다는 단점이 있지만 게시판의 경우 회원이 직접 생성할 수 없고,
+ * : 미리 정의해둔 값을 바탕으로 게시글을 생성하기에 Enum으로 정의해두었습니다.
+ */
 @Getter
 @RequiredArgsConstructor
 @JsonDeserialize(using = BoardTypeDeserializer.class)
